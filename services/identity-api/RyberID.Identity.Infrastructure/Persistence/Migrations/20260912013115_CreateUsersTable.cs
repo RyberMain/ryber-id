07947e5e0f25ef9fe8b538ba56da03cafef6d553
Mark Will Ccallo <120734906+MarkTWill@users.noreply.github.com>
2026-09-11 20:34:45 -0500

    feat: add initial user schema migration

---
+++ services/identity-api/RyberID.Identity.Infrastructure/Persistence/Migrations/20260912013115_CreateUsersTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RyberID.Identity.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
